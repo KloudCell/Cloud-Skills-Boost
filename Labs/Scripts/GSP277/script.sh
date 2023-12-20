b28990dc6eea01ddf7a3464de83c1a2d5bc68d93
KloudCell <154018008+KloudCell@users.noreply.github.com>
2023-12-20 23:26:52 +0530

    Commit & Update

---
+++ Labs/Scripts/GSP277/script.sh
@@ -0,0 +1,33 @@
+#! /bin/bash
+
+# Initialization
+gcloud init --skip-diagnostics
+
+export ID=$(gcloud config list --format 'value(core.project)')
+
+# Create Cloud Storage Bucket
+if (gsutil mb gs://$ID-bucket)
+
+then
+    printf "\n\e[1;96m%s\n\n\e[m" 'Created Bucket: Checkpoint Completed (1/3)'
+
+# Upload an image in a storage bucket (demo-image.jpg)
+    if (wget -O demo-image.jpg https://cdn.qwiklabs.com/3hpf8ZMmvpav2QvPqQCY1Zl1O%2B%2F8rrass6yjAPki3Dc%3D
+        gsutil cp demo-image.jpg gs://$ID-bucket)
+    
+    then
+        printf "\n\e[1;96m%s\n\n\e[m" 'Uploaded the image to Bucket: Checkpoint Completed (2/3)'
+
+# Make the uploaded image publicly accessible.
+        if (gsutil acl ch -u AllUsers:R gs://$ID-bucket/demo-image.jpg)
+        
+        then
+            printf "\n\e[1;96m%s\n\n\e[m" 'Image is now publicly accessible: Checkpoint Completed (3/3)'
+
+        fi
+    fi    
+fi
+
+printf "\n\e[1;92m%s\n\n\e[m" 'Lab Completed'
+
+gcloud auth revoke --all
