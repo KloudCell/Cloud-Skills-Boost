276b68e3c03ba9f94233fc87a785692d792da0c5
KloudCell <154018008+KloudCell@users.noreply.github.com>
2023-12-23 09:59:03 +0530

    Commit : Added ARC122 Cheatsheet

---
+++ Labs/Cheatsheets/ARC122/CHEATSHEET.md
@@ -0,0 +1,80 @@
+# **To be execute in Google Cloud Shell**
+
+**1. Create an API key.**
+
+    export BUCKET=$(gcloud info --format='value(config.project)')-bucket
+
+    export API_KEY=$(gcloud beta services api-keys create --display-name='API key 1' 2>&1 >/dev/null | grep -o 'keyString":"[^"]*' | cut -d'"' -f3)
+
+    # Get the ACL of the image
+    acl=$(gsutil acl get gs://$BUCKET/manif-des-sans-papiers.jpg)
+
+    # Check if the image is publicly available
+    if echo "$acl" | grep -q "allUsers"; then
+        echo "Your image is publicly available."
+    else
+        # Remove the image from the bucket
+        gsutil rm gs://$BUCKET/manif-des-sans-papiers.jpg
+
+        # Set image Publicly accessible
+        gsutil acl -r set public-read gs://$BUCKET
+
+        # Upload the image to the bucket
+        gsutil cp manif-des-sans-papiers.jpg gs://$BUCKET
+    fi
+
+    sleep  30    
+
+**2. Create and Update the json file**
+
+    echo '{
+      "requests": [
+          {
+            "image": {
+             "source": {
+                  "gcsImageUri": "gs://BUCKET/manif-des-sans-papiers.jpg"
+                }
+            },
+            "features": [
+              {
+                "type": "TEXT_DETECTION" ,
+                "maxResults": 10
+              }
+            ]
+        }
+      ]
+    }' > request.json
+
+    sed -i "s/BUCKET/$BUCKET/g" request.json
+
+    curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o text-response.json
+
+    gsutil cp text-response.json gs://$BUCKET
+
+    sleep 30
+
+**3. Update the json file to use the LANDMARK_DETECTION method**
+
+    echo '{
+    "requests": [
+        {
+            "image": {
+            "source": {
+                "gcsImageUri": "gs://BUCKET/manif-des-sans-papiers.jpg"
+            }
+            },
+            "features": [
+            {
+                "type": "LANDMARK_DETECTION" ,
+                "maxResults": 10
+            }
+            ]
+        }
+    ]
+    }' > request.json
+
+    sed -i "s/BUCKET/$BUCKET/g" request.json
+
+    curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o landmark-response.json
+
+    gsutil cp landmark-response.json gs://$BUCKET
--- Labs/Scripts/ARC122/script.sh
+++ Labs/Scripts/ARC122/script.sh
@@ -49,20 +49,31 @@ gcloud init --skip-diagnostics
 export BUCKET=$(gcloud info --format='value(config.project)')-bucket
 
 # Create an API KEY
-if (export API_KEY=$(gcloud beta services api-keys create --display-name='API key 1' 2>&1 >/dev/null | grep -o 'keyString":"[^"]*' | cut -d'"' -f3)
-
-    gsutil acl -r set public-read gs://$BUCKET
-
-    gsutil defacl ch -u AllUsers:R gs://$BUCKET
+if (export API_KEY=$(gcloud beta services api-keys create --display-name='API key 1' 2>&1 >/dev/null | grep -o 'keyString":"[^"]*' | cut -d'"' -f3) && sleep 30)
     
-    sleep 30)
-
 then
-    printf "\n\e[1;96m%s\n\n\e[m" 'API KEY Created: Checkpoint Completed (1/3)'
+    sleep 30 && printf "\n\e[1;96m%s\n\n\e[m" 'API KEY Created: Checkpoint Completed (1/3)'
+fi
 
+# Get the ACL of the image
+acl=$(gsutil acl get gs://$BUCKET/manif-des-sans-papiers.jpg)
+
+# Check if the image is publicly available
+if echo "$acl" | grep -q "allUsers"; then
+    echo "Your image is publicly available."
+else
+    # Remove the image from the bucket
+    gsutil rm gs://$BUCKET/manif-des-sans-papiers.jpg
+
+    # Download the image
+    wget https://github.com/KloudCell/Cloud-Skills-Boost/blob/main/Labs/Scripts/ARC122/manif-des-sans-papiers.jpg
+
+    # Upload the image to the bucket
+    gsutil cp manif-des-sans-papiers.jpg gs://$BUCKET
+fi
 
 # Create and Update the json file
-    if (echo '{
+echo '{
   "requests": [
       {
         "image": {
@@ -78,21 +89,20 @@ then
         ]
       }
   ]
-        }' > request.json
+}' > request.json
 
-        sed -i "s/BUCKET/$BUCKET/g" request.json
+sed -i "s/BUCKET/$BUCKET/g" request.json
 
-        curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o text-response.json
+curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o text-response.json
 
-        gsutil cp text-response.json gs://$BUCKET
-        
-        sleep 30)
-    then
-        printf "\n\e[1;96m%s\n\n\e[m" 'Updated the JSON File to use TEXT_DETECTION method: Checkpoint Completed (2/3)'
+if (gsutil cp text-response.json gs://$BUCKET)
 
+then
+    sleep 30 && printf "\n\e[1;96m%s\n\n\e[m" 'Updated the JSON File to use TEXT_DETECTION method: Checkpoint Completed (2/3)'
+fi
 
 # Update the json file to use the LANDMARK_DETECTION method
-        if (echo '{
+echo '{
   "requests": [
       {
         "image": {
@@ -108,18 +118,16 @@ then
         ]
       }
   ]
-            }' > request.json
+}' > request.json
 
-            sed -i "s/BUCKET/$BUCKET/g" request.json
+sed -i "s/BUCKET/$BUCKET/g" request.json
 
-            curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o landmark-response.json
+curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json  https://vision.googleapis.com/v1/images:annotate?key=${API_KEY} -o landmark-response.json
 
-            gsutil cp landmark-response.json gs://$BUCKET)
-        then
-            printf "\n\e[1;96m%s\n\n\e[m" 'Updated the JSON File to use LANDMARK_DETECTION method: Checkpoint Completed (3/3)'
+if (gsutil cp landmark-response.json gs://$BUCKET)
 
-        fi
-    fi    
+then
+    printf "\n\e[1;96m%s\n\n\e[m" 'Updated the JSON File to use LANDMARK_DETECTION method: Checkpoint Completed (3/3)'
 fi
 
 printf "\n\e[1;92m%s\n\n\e[m" 'Lab Completed'
