09ba2e603fcf5709c9dc8c5b77dae870ba90b515
KloudCell <154018008+KloudCell@users.noreply.github.com>
2023-12-23 16:11:40 +0530

    Add: Common Code

---
+++ resources/common_code.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+export ID=$(gcloud info --format='value(config.project)')
+
+export ZONE=$(gcloud compute project-info describe --project $ID --format="value(commonInstanceMetadata.items.google-compute-default-zone)")
+
+export REGION=${ZONE::-2}
+
+export API_KEY=$(gcloud beta services api-keys create --display-name='API key 1' 2>&1 >/dev/null | grep -o 'keyString":"[^"]*' | cut -d'"' -f3)
+
+BRIGHT_RED='\033[1;31m'
+NC='\033[0m' # No Color
+
+echo -e "Your Default PROJECT ID: ${BRIGHT_RED}$ID${NC}"
+echo -e "Your Default ZONE      : ${BRIGHT_RED}$ZONE${NC}"
+echo -e "Your Default REGION    : ${BRIGHT_RED}$REGION${NC}"
+echo -e "Your API KEY		: ${BRIGHT_RED}$API_KEY${NC}"
+
