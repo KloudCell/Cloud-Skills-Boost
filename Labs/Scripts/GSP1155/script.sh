d94af2573d9c7b390d012a8fd3c4e6455bb74de0
KloudCell <154018008+KloudCell@users.noreply.github.com>
2024-01-13 11:18:31 +0530

    Update script.sh

--- Labs/Scripts/GSP1155/script.sh
+++ Labs/Scripts/GSP1155/script.sh
@@ -9,6 +9,20 @@ gcloud init --skip-diagnostics
 wget https://raw.githubusercontent.com/KloudCell/Cloud-Skills-Boost/main/resources/common_code.sh 2> /dev/null
 source common_code.sh
 
+while true; do
+    # Print the message
+    echo -e "Please accept the terms by going to the following link otherwise lab will not complete: \033[0;34mhttps://console.developers.google.com/terms/cloud\033[0m"
+    
+    # Ask for user input
+    read -p "Have you accepted the terms? (y/n) " yn
+    
+    # Check the user input
+    case $yn in
+        [Yy]* ) break;;
+        * ) echo "Please accept the terms to proceed.";;
+    esac
+done
+
 # Enable the Vertex AI API
 if (gcloud services enable aiplatform.googleapis.com)
 
