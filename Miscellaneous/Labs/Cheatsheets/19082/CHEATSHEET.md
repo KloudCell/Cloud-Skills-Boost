0ab79385e0d5ccc8dba0ba2a6ea5646e1a76530d
KloudCell <154018008+KloudCell@users.noreply.github.com>
2024-02-04 19:56:07 +0530

    Create: 19082

---
+++ Miscellaneous/Labs/Cheatsheets/19082/CHEATSHEET.md
@@ -0,0 +1,45 @@
+# **To be done using Google Cloud Shell**
+
+**1. Create a bucket and upload a sample file**
+
+**2. Remove project access**
+
+**3. Add storage permissions**
+
+**4. Set up the Service Account User and create a VM**
+
+```
+wget https://raw.githubusercontent.com/KloudCell/Cloud-Skills-Boost/main/resources/common_code.sh 2> /dev/null
+. common_code.sh
+
+gsutil mb -l us gs://$ID
+
+touch sample.txt
+
+gsutil cp sample.txt gs://$ID
+
+read -p "Username 2:" USER_2
+
+gcloud projects remove-iam-policy-binding $ID --member=user:$USER_2 --role=roles/viewer
+
+gcloud projects add-iam-policy-binding $ID \
+  --role=roles/storage.objectViewer \
+  --member=user:$USER_2
+
+gcloud iam service-accounts create read-bucket-objects --display-name "read-bucket-objects" 
+
+gcloud iam service-accounts add-iam-policy-binding  read-bucket-objects@$ID.iam.gserviceaccount.com --member=domain:altostrat.com --role=roles/iam.serviceAccountUser
+
+gcloud projects add-iam-policy-binding $ID --member=domain:altostrat.com --role=roles/compute.instanceAdmin.v1
+
+gcloud projects add-iam-policy-binding $ID --member="serviceAccount:read-bucket-objects@$ID.iam.gserviceaccount.com" --role="roles/storage.objectViewer"
+
+gcloud compute instances create demoiam \
+  --zone=$ZONE \
+  --machine-type=e2-micro \
+  --image-family=debian-11 \
+  --image-project=debian-cloud \
+  --service-account=read-bucket-objects@$ID.iam.gserviceaccount.com
+```
+
+## Lab Completed🎉
---
+++ Miscellaneous/Labs/Scripts/19082/script.sh
@@ -0,0 +1,67 @@
+#! /bin/bash
+
+wget https://raw.githubusercontent.com/KloudCell/Cloud-Skills-Boost/main/resources/welcome.sh 2> /dev/null
+. welcome.sh
+
+# Initialization
+gcloud init --skip-diagnostics
+
+wget https://raw.githubusercontent.com/KloudCell/Cloud-Skills-Boost/main/resources/common_code.sh 2> /dev/null
+. common_code.sh
+
+# Create a bucket and upload a sample file
+
+if (gsutil mb -l us gs://$ID
+
+touch sample.txt
+
+gsutil cp sample.txt gs://$ID)
+
+then
+    printf "\n\e[1;96m%s\n\n\e[m" 'Bucket Created & File Uploaded: Checkpoint Completed (1/4)'
+
+# Remove project access
+
+    if (read -p "Username 2:" USER_2
+
+    gcloud projects remove-iam-policy-binding $ID --member=user:$USER_2 --role=roles/viewer)
+
+    then
+        printf "\n\e[1;96m%s\n\n\e[m" 'Access Removed: Checkpoint Completed (2/4)'
+
+# Add storage permissions
+
+        if (gcloud projects add-iam-policy-binding $ID \
+        --role=roles/storage.objectViewer \
+        --member=user:$USER_2)
+
+        then
+            printf "\n\e[1;96m%s\n\n\e[m" 'Storage Permission Added: Checkpoint Completed (3/4)'
+
+# Set up the Service Account User and create a VM
+
+            if (gcloud iam service-accounts create read-bucket-objects --display-name "read-bucket-objects" 
+
+            gcloud iam service-accounts add-iam-policy-binding  read-bucket-objects@$ID.iam.gserviceaccount.com --member=domain:altostrat.com --role=roles/iam.serviceAccountUser
+
+            gcloud projects add-iam-policy-binding $ID --member=domain:altostrat.com --role=roles/compute.instanceAdmin.v1
+
+            gcloud projects add-iam-policy-binding $ID --member="serviceAccount:read-bucket-objects@$ID.iam.gserviceaccount.com" --role="roles/storage.objectViewer"
+
+            gcloud compute instances create demoiam \
+            --zone=$ZONE \
+            --machine-type=e2-micro \
+            --image-family=debian-11 \
+            --image-project=debian-cloud \
+            --service-account=read-bucket-objects@$ID.iam.gserviceaccount.com)
+
+            then
+                printf "\n\e[1;96m%s\n\n\e[m" 'Service Account Set Up & VM Created : Checkpoint Completed (4/4)'
+            fi     
+        fi
+    fi
+fi
+
+printf "\n\e[1;92m%s\n\n\e[m" 'Lab Completed'
+
+gcloud auth revoke --all
